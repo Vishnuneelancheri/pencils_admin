abf6482ecc4afbb65569d25dc383c65f944881bc
LAB <vishnuvishnuneelan@gmail.com>
2018-04-02 13:33:19 +0530

    First commit

---
+++ app/src/main/java/com/mainproject/vishnu_neelancheri/pencilsadmin/cancellation/ViewCancellationModel.java
@@ -0,0 +1,9 @@
+package com.mainproject.vishnu_neelancheri.pencilsadmin.cancellation;
+
+/**
+ * Created by Vishnu Neelancheri, email: vishnuvishnuneelan@gmail.com on 4/2/2018
+ */
+
+public class ViewCancellationModel {
+
+}
